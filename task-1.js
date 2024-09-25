c2fc8347819912f84ce0e45c89032806e78ab129
CodeWithShaman <shamanaliteevino@gmail.com>
2024-09-25 12:30:24 +0500

    pattrnn with javascript

---
+++ task-1.js
@@ -0,0 +1,10 @@
+for(let i = 13; i>=1; i = i-2){
+
+    for(let k = 13; k>=i; k--){
+     document.write("&nbsp")
+}
+    for(let j = 1; j<=i; j++){
+        document.write("*")
+    }
+   document.write("<br>")  
+}
